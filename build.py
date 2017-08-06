a319115024606efe7669827c24e3ceaad469c752
dave sullivan <dave.brian.sullivan@gmail.com>
2017-08-05 20:14:04 -0700

    better filenames, fix css issue

--- build.py
+++ build.py
@@ -8,6 +8,9 @@ SITE_PREFIX = ''
 post_filenames = sorted(os.listdir('posts'))
 post_filenames.reverse()
 
+# they are ordered correctly, so strip out the ugly digits at the front for ordering
+clean_names = [x[4:] for x in post_filenames]
+
 post_listing_contexts = []
 
 # load post template, we'll render 1 for each post found in posts directory
@@ -15,15 +18,15 @@ post_template_filename = 'post.html'
 loader = jinja2.FileSystemLoader('templates/')
 template = jinja2.Environment(loader=loader).get_template(post_template_filename)
 
-for post_filename in post_filenames:
+for post_filename, clean_filename in zip(post_filenames, clean_names):
     lines = open('posts/'+post_filename).readlines()
-    content_html = markdown.markdown(''.join(lines), 
+    content_html = markdown.markdown(unicode(''.join(lines), 'utf-8'),
             extensions=['markdown.extensions.codehilite', 'markdown.extensions.fenced_code'])
 
     # we'll use these to render the index page after writing out each post
     post_listing_context = {
                 'title': lines[0][1:],
-                'url': SITE_PREFIX+post_filename[:-3]+'.html'
+                'url': SITE_PREFIX+clean_filename[:-3]+'.html'
             }
     post_listing_contexts.append(post_listing_context)
 
@@ -31,8 +34,8 @@ for post_filename in post_filenames:
                 'content': content_html
             }
     rendered_post = template.render(post_content_context)
-    with open("build/"+post_filename[:-3]+'.html', "wb") as fh:
-            fh.write(rendered_post)
+    with open("build/"+clean_filename[:-3]+'.html', "wb") as fh:
+            fh.write(rendered_post.encode('utf-8'))
 
 # render index
 filename = 'index.html'
